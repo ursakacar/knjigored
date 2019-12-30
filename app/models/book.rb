46349d8341004c9f4b4525c07aaa78f3f5e6726a
Ursa Kacar <kacar.ursa@gmail.com>
2019-12-30 21:00:13 +0100

    added validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :genre
   belongs_to :author
+
+  validates :title, :internal_number, presence: true
+  validates :internal_number, numericality: { only_integer: true }
+  validates :internal_number, uniqueness: true
 end
