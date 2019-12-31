304d8fd209842cc03c0c559ab95ad5fbcf45b27b
Ursa Kacar <kacar.ursa@gmail.com>
2019-12-31 14:09:02 +0100

    enter key support for search

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,7 +1,15 @@
+var searchFunction = function () {
+  const searchInput = document.getElementById('books-search-input').value
+  const destination = `/knjige?query=${searchInput}`
+  window.location.href = destination
+}
+
 window.addEventListener('load',function () {
-  document.getElementById('books-search-button').addEventListener('click', function () {
-    const searchInput = document.getElementById('books-search-input').value
-    const destination = `/knjige?query=${searchInput}`
-    window.location.href = destination
+  document.getElementById('books-search-button').addEventListener('click', searchFunction)
+  document.getElementById('books-search-input').addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      searchFunction()
+    }
   })
 })
+
