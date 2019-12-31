35292dcbc78bd87d9f4abe0847297885dba36add
Ursa Kacar <kacar.ursa@gmail.com>
2019-12-31 15:39:37 +0100

    seed db with genres, sort genres and authors

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,7 +4,7 @@ class GenresController < ApplicationController
   # GET /genres
   # GET /genres.json
   def index
-    @genres = Genre.all
+    @genres = Genre.all.order(:name)
   end
 
   # GET /genres/1
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Inserting genres'
+
+Genre.new(name: 'biografija').save!
+Genre.new(name: 'drama').save!
+Genre.new(name: 'fantazija').save!
+Genre.new(name: 'kratka zgodba').save!
+Genre.new(name: 'kriminalka').save!
+Genre.new(name: 'ljubezenski roman').save!
+Genre.new(name: 'mladinski roman').save!
+Genre.new(name: 'modernistični roman').save!
+Genre.new(name: 'pesmi').save!
+Genre.new(name: 'potopis').save!
+Genre.new(name: 'pravljica').save!
+Genre.new(name: 'psihološki roman').save!
+Genre.new(name: 'vojni roman').save!
+Genre.new(name: 'zgodovinski roman').save!
