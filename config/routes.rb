c08802f6de097a5b943707e36fd97e97e0290dc0
Ursa Kacar <kacar.ursa@gmail.com>
2019-12-31 14:47:12 +0100

    added home page path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :books, path: "knjige"
   resources :genres, path: "zanri"
   resources :authors, path: "avtorji"
+
+  get '', to: redirect('/knjige')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
