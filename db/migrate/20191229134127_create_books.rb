01a7a844c1a524fad53f2b460ba8cef719ef67e8
Ursa Kacar <kacar.ursa@gmail.com>
2019-12-31 14:59:12 +0100

    fixed db migration

--- db/migrate/20191229134127_create_books.rb
+++ db/migrate/20191229134127_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
     create_table :books do |t|
       t.integer :internal_number
       t.string :title
-      t.boolean :is_borrowed
+      t.boolean :is_available
       t.references :genre, null: false, foreign_key: true
       t.references :author, null: false, foreign_key: true
 
