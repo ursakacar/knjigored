a641642ecc9ce0c3b49101216dfc0c1fcbe1e702
Ursa Kacar <kacar.ursa@gmail.com>
2021-02-06 15:54:57 +0100

    adds tests for author model

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save author without a name" do
+    author = Author.new(
+    )
+    assert_not author.save
+    assert_equal "Ime ne sme biti prazno", author.errors.full_messages.first
+  end
+
+  test "should not save author with duplicate name" do
+    author = Author.new(
+      name: "Nov Avtor",
+    )
+    author_duplicated = Author.new(
+      name: "Nov Avtor",
+    )
+    assert author.save
+    assert_not author_duplicated.save
+    assert "Nov Avtor", author.name
+    assert_equal "Ime že obstaja", author_duplicated.errors.full_messages.first
+  end
+
+  test "should save author with name" do
+    author = Author.new(
+      name: "Nov Avtor",
+    )
+    assert author.save
+    assert "Nov Avtor", author.name
+  end
+
 end
