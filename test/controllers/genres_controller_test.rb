0a795ff10d1baeca441b6df54c7926cff812d910
Ursa Kacar <kacar.ursa@gmail.com>
2021-02-08 13:32:08 +0100

    adds tests for genre controller and necessary code changes to make them pass

--- test/controllers/genres_controller_test.rb
+++ test/controllers/genres_controller_test.rb
@@ -3,46 +3,130 @@ require 'test_helper'
 class GenresControllerTest < ActionDispatch::IntegrationTest
   setup do
     @genre = genres(:one)
+    @genre2 = genres(:two)
   end
 
-  test "should get index" do
+  test "should get index if logged in" do
+    login
     get genres_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_genre_url
-    assert_response :success
+  test "should not get index if not logged in" do
+    get genres_url
+    assert_response :redirect
+    assert_redirected_to new_session_path
   end
 
-  test "should create genre" do
-    assert_difference('Genre.count') do
-      post genres_url, params: { genre: { name: @genre.name } }
-    end
-
-    assert_redirected_to genre_url(Genre.last)
-  end
-
-  test "should show genre" do
+  test "should show genre if logged in" do
+    login
     get genre_url(@genre)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not show genre if not logged in" do
+    get genre_url(@genre)
+    assert_response :redirect
+    assert_redirected_to new_session_path
+  end
+
+  test "should get new if logged in" do
+    login
+    get new_genre_url
+    assert_response :success
+  end
+
+  test "should not get new if not logged in" do
+    get new_genre_url
+    assert_response :redirect
+    assert_redirected_to new_session_path
+  end
+
+  test "should get edit if logged in" do
+    login
     get edit_genre_url(@genre)
     assert_response :success
   end
 
-  test "should update genre" do
-    patch genre_url(@genre), params: { genre: { name: @genre.name } }
-    assert_redirected_to genre_url(@genre)
+  test "should not get edit if not logged in" do
+    get edit_genre_url(@genre)
+    assert_response :redirect
+    assert_redirected_to new_session_path
   end
 
-  test "should destroy genre" do
+  test "should create genre if logged in" do
+    login
+    assert_difference('Genre.count') do
+      post genres_url, params: { genre: { name: 'genre' } }
+    end
+
+    assert_redirected_to genre_url(Genre.last)
+
+    created_genre = Genre.all.order(created_at: :desc).first
+    assert_equal created_genre.name, "genre"
+  end
+
+  test "should not create genre if not logged in" do
+    assert_no_difference('Genre.count') do
+      post genres_url, params: { genre: { name: 'genre' } }
+    end
+
+    assert_response :redirect
+    assert_redirected_to new_session_path
+  end
+
+  test "should update genre if logged in" do
+    login
+    patch genre_url(@genre), params: { genre: { name: 'genre' } }
+    assert_redirected_to genre_url(@genre)
+
+    updated_genre = Genre.find(@genre.id)
+    assert_equal updated_genre.name, "genre"
+  end
+
+  test "should not update genre with duplicate name if logged in" do
+    login
+    patch genre_url(@genre), params: { genre: { name: @genre2.name } }
+    updated_genre = Genre.find(@genre.id)
+    assert_equal updated_genre.name, @genre.name
+  end
+
+  test "should not update genre if not logged in" do
+    patch genre_url(@genre), params: { genre: { name: 'genre' } }
+    assert_response :redirect
+    assert_redirected_to new_session_path
+
+    updated_genre = Genre.find(@genre.id)
+    assert_equal updated_genre.name, @genre.name
+  end
+
+  test "should destroy genre if logged in" do
+    login
+    post genres_url, params: { genre: { name: 'genre' } }
+    created_genre = Genre.all.order(created_at: :desc).first
+
     assert_difference('Genre.count', -1) do
+      delete genre_url(created_genre)
+    end
+
+    assert_redirected_to genres_url
+  end
+
+  test "should not destroy genre with books if logged in" do
+    login
+    assert_no_difference('Genre.count') do
       delete genre_url(@genre)
     end
 
     assert_redirected_to genres_url
+  end
+
+  test "should not destroy genre if not logged in" do
+    assert_no_difference('Genre.count') do
+      delete genre_url(@genre)
+    end
+
+    assert_response :redirect
+    assert_redirected_to new_session_path
   end
 end
